73c97aa04023db686d21a036a34e8241ee667e84
Thomas Forselius <forselius.thomas@gmail.com>
2022-12-10 00:20:51 +0100

    allowed_hosts

--- api/settings.py
+++ api/settings.py
@@ -18,16 +18,12 @@ if os.path.exists('env.py'):
     import env
 
 CLOUDINARY_STORAGE = { 'CLOUDINARY_URL':
-    os.environ.get('CLOUDINARY_URL') }
+    os.environ.get('CLOUDINARY_URL') 
+}
 MEDIA_URL = '/bokning/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -35,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lghbokning.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['lghbokning.herokuapp.com']
 
 
 # Application definition
