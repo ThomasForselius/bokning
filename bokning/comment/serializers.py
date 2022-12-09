6fa3ca12ff8b3261b2d804d1a19ad8f9a42d1b96
Thomas Forselius <forselius.thomas@gmail.com>
2022-12-09 20:32:23 +0100

    added natural time to comment serializer

--- bokning/comment/serializers.py
+++ bokning/comment/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from rest_framework.response import Response
 from .models import Comment
+from django.contrib.humanize.templatetags.humanize import naturaltime
+
 
 class CommentSerializer(serializers.ModelSerializer): 
     owner = serializers.ReadOnlyField(source='owner.username')
     booking = serializers.ReadOnlyField(source='booking.date')
     is_owner = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get(self, request):
         serializer = CommentSerializer(Comment, many= True) 
@@ -14,6 +18,12 @@ class CommentSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj): 
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+    
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     class Meta: 
         model = Comment 
