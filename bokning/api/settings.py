2ff66131fee1e5ffb03ee16f0e71dfed4d922de5
Thomas Forselius <forselius.thomas@gmail.com>
2022-12-09 20:30:59 +0100

    added root route and jwt auth to settings

--- bokning/api/settings.py
+++ bokning/api/settings.py
@@ -49,12 +49,40 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
 
     'comment',
     'booking',
     'profiles',
     
 ]
+SITE_ID = 1
+REST_FRAMEWORK = { 
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
+}
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CL ASSES'] = [
+        'restframework.renderers.JSONRenderer' ]
+REST_USE_JWT = True
+JWT_AUTH_COOKIE = 'booking-auth'
+JWT_AUTH_SECURE = True
+JWT_AUTH_REFRESH_COOKIE = 'booking-refresh-token'
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'api.serializers.CurrentUserSerializer'
+    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
