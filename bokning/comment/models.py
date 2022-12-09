8e0df65f2034c50e7b4958264856f0d16648e69d
Thomas Forselius <forselius.thomas@gmail.com>
2022-12-09 19:40:54 +0100

    added perform_create to view

--- bokning/comment/models.py
+++ bokning/comment/models.py
@@ -4,10 +4,10 @@ from django.db.models.signals import post_save
 from booking.models import Booking
 
 class Comment(models.Model):
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
     booking = models.ForeignKey(Booking,
                              on_delete=models.CASCADE,
-                             related_name='comment')
+                             related_name='booking')
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     desc = models.CharField(max_length=255, blank=True)
--- bokning/comment/views.py
+++ bokning/comment/views.py
@@ -4,11 +4,14 @@ from api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer
 
-class CommentList(generics.ListAPIView):
+class CommentList(generics.ListCreateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
     
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve or update a comment if you're the owner.
