ad11e455288d87ab9e9898cf2f697bbe92b3b11d
Thomas Forselius <forselius.thomas@gmail.com>
2023-01-12 08:11:16 +0100

    added filtering to bookings

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,5 @@
 # pylint: disable=no-member
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from api.permissions import IsOwnerOrReadOnly
 from .models import Booking
 from .serializers import BookingSerializer
@@ -8,6 +8,7 @@ class BookingList(generics.ListCreateAPIView):
     serializer_class = BookingSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Booking.objects.all().order_by('date')
+    
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
@@ -15,6 +16,10 @@ class BookingList(generics.ListCreateAPIView):
     search_fields = [
         'date',
         'owner',
+    ]
+    ordering_fields = [
+        'owner__username',
+        'date'
     ]
 
     def perform_create(self, serializer):
